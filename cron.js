300850b5c99fcdd9003d490cbcb029cecbe22d0a
SrinivasCresol <138846621+SrinivasCresol@users.noreply.github.com>
2023-12-08 15:18:00 +0530

    Updated Cron Jobs

--- cron.js
+++ cron.js
@@ -1,7 +1,7 @@
 const cron = require("cron");
 const https = require("https");
 
-const backendUrl = "https://ecommerce-web-app-backend.onrender.com/";
+const backendUrl = "https://sample-ecommerce-3kq2.onrender.com/";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   console.log("Restarting Server");
